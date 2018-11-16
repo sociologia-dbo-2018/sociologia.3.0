07e2db6599f63e7a06374af1eff050ea855b4fbf
Diego Costa <36934264+costafsk@users.noreply.github.com>
2018-11-16 00:59:51 -0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+import {getNews} from './modules/rss.module.js';
+
+document.addEventListener('DOMLoadedContent', init());
+
+function init() {
+    // Carregando o Feed Principal do mapa
+
+    // Carregando RSS Global
+    getNews();
+    // Carregando RSS Local
+    
+    // Carregando 
+}
+
+
+
