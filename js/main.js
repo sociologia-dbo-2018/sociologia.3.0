fc8b5abe8fbb6a7af087c52451cd013c1dbe484e
cassianasl <cassianasilveira64@gmail.com>
2018-11-27 13:25:38 -0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import {getNews} from '../modules/rss.module.js';
+// import {formControl} from './modules/formControl.module.js';
+
+document.addEventListener('DOMContentLoaded', getNews);
+
+// function init() {
+//     // Carregando o Feed Principal do mapa
+
+//     // Carregando RSS Global
+//     getNews();
+//     // Carregando RSS Local
+
+//     // Alguns Eventos
+// }
